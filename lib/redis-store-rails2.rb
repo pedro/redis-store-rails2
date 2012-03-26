25b927a6526b8c2ed56da35a2e5059e552bf52b4
Pedro Belo <pedro@heroku.com>
2012-03-25 18:50:48 -0700

    add increment/decrement and some error handling

--- lib/redis-store-rails2.rb
+++ lib/redis-store-rails2.rb
@@ -15,6 +15,9 @@ class RedisStoreRails2 < ActiveSupport::Cache::Store
   def read(key, options = nil) # :nodoc:
     super
     @store.get(key)
+  rescue Errno::ECONNREFUSED => e
+    logger.error("RedisStoreRails2 error (#{e}): #{e.message}")
+    nil
   end
 
   def write(key, value, options={})
@@ -22,5 +25,26 @@ class RedisStoreRails2 < ActiveSupport::Cache::Store
     response = @store.set(key, value)
     @store.expire(key, options[:expires_in]) if options[:expires_in]
     response == "OK"
+  rescue Errno::ECONNREFUSED => e
+    logger.error("RedisStoreRails2 error (#{e}): #{e.message}")
+    false
+  end
+
+  def delete(key, options={})
+    super
+    response = @store.del(key)
+    response >= 0
+  end
+
+  def increment(key, amount = 1)
+    @store.incrby key, amount
+  end
+
+  def decrement(key, amount = 1)
+    @data.decrby key, amount
+  end
+
+  def clear
+    @store.flushdb
   end
 end
