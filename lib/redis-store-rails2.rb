c0674557a92882f96a1feb94a92bad0479a403df
Pedro Belo <pedro@heroku.com>
2012-03-28 16:13:17 -0700

    do not fail if can't connect to redis on boot

--- lib/redis-store-rails2.rb
+++ lib/redis-store-rails2.rb
@@ -9,24 +9,27 @@ class RedisStoreRails2 < ActiveSupport::Cache::Store
     address ||= "redis://localhost:6379"
 
     uri = URI.parse(address)
-    options  = { :host => uri.host, :port => uri.port }
     password = uri.password || uri.user
-    options.merge!(:password => password) if password
-    @store = Redis.new(options)
+    @options  = { :host => uri.host, :port => uri.port }
+    @options.merge!(:password => password) if password
+
+    handle_errors do
+      store # attempt to connect to Redis on boot
+    end
   end
 
   def read(key, options = {})
     super
     handle_errors(options) do
-      @store.get(key)
+      store.get(key)
     end
   end
 
   def write(key, value, options={})
     super
     handle_errors({ :default_value => false }.merge(options)) do
-      response = @store.set(key, value)
-      @store.expire(key, options[:expires_in]) if options[:expires_in]
+      response = store.set(key, value)
+      store.expire(key, options[:expires_in]) if options[:expires_in]
       response == "OK"
     end
   end
@@ -34,34 +37,38 @@ class RedisStoreRails2 < ActiveSupport::Cache::Store
   def delete(key, options={})
     super
     handle_errors({ :default_value => false }.merge(options)) do
-      response = @store.del(key)
+      response = store.del(key)
       response >= 0
     end
   end
 
   def increment(key, amount = 1)
     handle_errors(options) do
-      return nil unless @store.exists(key)
-      @store.incrby key, amount
+      return nil unless store.exists(key)
+      store.incrby key, amount
     end
   end
 
   def decrement(key, amount = 1)
     handle_errors(options) do
-      return nil unless @store.exists(key)
+      return nil unless store.exists(key)
       @data.decrby key, amount
     end
   end
 
   def clear
     handle_errors(options) do
-      @store.flushdb
+      store.flushdb
     end
   end
 
   protected
 
-  def handle_errors(options)
+  def store
+    @store ||= Redis.new(@options)
+  end
+
+  def handle_errors(options = {})
     return yield
   rescue Errno::ECONNREFUSED => e
     logger.error("RedisStoreRails2 error (#{e.class.name}): #{e.message}")
